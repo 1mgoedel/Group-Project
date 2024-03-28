2179d29dd732f460995f642672f237788b35e0cb
Lucas0818 <lucasgsiri@email.com>
2024-03-28 17:31:28 -0400

    password

--- movie_hub/flask_app/config/mysqlconnection.py
+++ movie_hub/flask_app/config/mysqlconnection.py
@@ -4,7 +4,7 @@ class MySQLConnection:
     def __init__(self, db):
         connection = pymysql.connect(host = 'localhost',
                                     user = 'root', 
-                                    password = 'Tek01010100', 
+                                    password = 'root', 
                                     db = db,
                                     charset = 'utf8mb4',
                                     cursorclass = pymysql.cursors.DictCursor,
@@ -15,7 +15,7 @@ class MySQLConnection:
             try:
                 query = cursor.mogrify(query, data)
                 print("Running Query:", query)
-     
+
                 cursor.execute(query)
                 if query.lower().find("insert") >= 0:
                     self.connection.commit()
